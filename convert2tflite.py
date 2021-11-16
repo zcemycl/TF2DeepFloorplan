8487d34e194ce4b983a9ad5063ed42287f27b73f
zcemycl <yui.leung.15@ucl.ac.uk>
2021-11-16 21:22:25 +0000

    provide quantization option to convert2tflite.py

--- convert2tflite.py
+++ convert2tflite.py
@@ -4,6 +4,8 @@ import argparse
 def converter(config):
     model=tf.keras.models.load_model(config.modeldir)
     converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    if not config.quantize:
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
     converter.experimental_new_converter = True
     tflite_model = converter.convert()
     with open(config.tflitedir,'wb') as f:
@@ -14,6 +16,7 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument('--modeldir',type=str,default='model/store')
     p.add_argument('--tflitedir',type=str,default='model/store/model.tflite')
+    p.add_argument('--quantize',action='store_true')
     args = p.parse_args()
     converter(args)
 
