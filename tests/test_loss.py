b491edc1456ebe6560c70b16dae2531f51cae431
zcemycl <yui.leung.15@ucl.ac.uk>
2022-06-14 23:51:19 +0100

    loss.py and net.py testings

---
+++ tests/test_loss.py
@@ -0,0 +1,21 @@
+import unittest
+
+import tensorflow as tf
+
+from dfp.loss import balanced_entropy, cross_two_tasks_weight
+
+
+class TestLossCase(unittest.TestCase):
+    randx = tf.random.normal((1, 32, 32, 3), 0, 1)
+    randy = tf.random.normal((1, 32, 32, 9), 0, 1)
+
+    def test_balanced_entropy(self):
+        loss = balanced_entropy(self.__class__.randx, self.__class__.randx)
+        self.assertEqual(loss.dtype, tf.float32)
+
+    def test_weight(self):
+        w1, w2 = cross_two_tasks_weight(
+            self.__class__.randx, self.__class__.randy
+        )
+        w = w1 + w2
+        self.assertLessEqual(w.numpy(), 1.0)
